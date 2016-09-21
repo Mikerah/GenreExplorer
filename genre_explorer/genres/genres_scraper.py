bc5d284a7b2714e53237d1ee118caaf75eb9662b
Mikerah <mikerah14@gmail.com>
2016-09-21 19:06:25 -0400

    refactored code into functions

--- genre_explorer/genres/genres_scraper.py
+++ genre_explorer/genres/genres_scraper.py
@@ -1,45 +1,65 @@
-import bs4, requests, re, string, collections
+import bs4, requests, string
 
-list_of_letters = list(string.ascii_uppercase)
-list_of_letters.append("#")
-genres = {k: [] for k in list_of_letters}
-
-page_text = requests.get("https://en.wikipedia.org/wiki/List_of_music_styles")
-try:
-    page_text.raise_for_status
-except Exception as exc:
-    print("There was a problem")
-    
-page_text = page_text.text
-bs_obj = bs4.BeautifulSoup(page_text, "html.parser")
-links_to_genres = bs_obj.findAll(href=re.compile("List_of_styles_of_music:_[A-Z].[A-Z]"))
-links = []
-for i in links_to_genres:
-    links.append("https://en.wikipedia.org" + i.get("href"))
-    
-g = []
-for i in links:
-    wiki_genres_page = requests.get(i)
+def get_links_to_sections_of_genres():
+    """
+    Returns a list of links to the different sections of genres in wikipedia
+    """
+    page_text = requests.get("https://en.wikipedia.org/wiki/List_of_music_styles")
     try:
-        wiki_genres_page.raise_for_status
+        page_text.raise_for_status
     except Exception as exc:
         print("There was a problem")
-    wiki_genres_page = wiki_genres_page.text
-    bs_obj = bs4.BeautifulSoup(wiki_genres_page, "html.parser")
-    for div in bs_obj.findAll("div", {"class": "navbox"}):
-        div.decompose()
-    bs_obj.p.decompose()
-    
-    tmp1 = bs_obj.select(".mw-content-ltr ul li")
-    for i in tmp1:
-        g.append(str(i.next_element.next_element))
-    
-    del g[len(g)-1]
-    
-for genre in g:
-    if genre[0] in genres:
-        genres[genre[0]].append(genre)
-    else:
-        genres["#"].append(genre)
+        
+    page_text = page_text.text
+    bs_obj = bs4.BeautifulSoup(page_text, "html.parser")
+    links_to_genres = bs_obj.findAll(href=re.compile("List_of_styles_of_music:_[A-Z].[A-Z]"))
+    links = []
+    for i in links_to_genres:
+        links.append("https://en.wikipedia.org" + i.get("href"))
+        
+    return links
    
-print(genres)
+
+def get_list_of_genre_from_links(links):
+    """
+    Returns the list of genres from the given list of links
+    :params links - list
+    """
+    g = []
+    for i in links:
+        wiki_genres_page = requests.get(i)
+        try:
+            wiki_genres_page.raise_for_status
+        except Exception as exc:
+            print("There was a problem")
+        wiki_genres_page = wiki_genres_page.text
+        bs_obj = bs4.BeautifulSoup(wiki_genres_page, "html.parser")
+        for div in bs_obj.findAll("div", {"class": "navbox"}):
+            div.decompose()
+        bs_obj.p.decompose()
+        
+        tmp1 = bs_obj.select(".mw-content-ltr ul li")
+        for i in tmp1:
+            g.append(str(i.next_element.next_element))
+        
+        del g[len(g)-1]
+        
+    return g
+    
+def create_genres_dictionary(list_of_genres):
+    """
+    Returns a dictionary of genres where the keys are the alphabet and the values
+    a list of genres
+    :params list_of_genres - list
+    """
+    list_of_letters = list(string.ascii_uppercase)
+    list_of_letters.append("#")
+    genres = {k: [] for k in list_of_letters}
+    
+    for genre in list_of_genres:
+        if genre[0] in genres:
+            genres[genre[0]].append(genre)
+        else:
+            genres["#"].append(genre)
+            
+    return genres
