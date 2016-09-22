e5c2393a8bc900a4412ecaba8467c92b81bb3f9a
Mikerah <mikerah14@gmail.com>
2016-09-22 11:39:05 -0400

    Applied migrations for Genres model

--- genre_explorer/genres/models.py
+++ genre_explorer/genres/models.py
@@ -2,4 +2,4 @@ from django.db import models
 
 class Genre(models.Model):
     name = models.CharField(max_length=100)
-    playlist_url = models. CharField(max_length=250)
+    playlist_url = models.URLField()
