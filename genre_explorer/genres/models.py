9afd30c1b75750405bb551f41916d3c2d558fbc3
Mikerah <mikerah14@gmail.com>
2016-09-21 20:09:17 -0400

    completed genre model in models.py

--- genre_explorer/genres/models.py
+++ genre_explorer/genres/models.py
@@ -1,3 +1,5 @@
 from django.db import models
 
-# Create your models here.
+class Genre(models.Model):
+    name = models.CharField(max_length=100)
+    playlist_url = models. CharField(max_length=250)
