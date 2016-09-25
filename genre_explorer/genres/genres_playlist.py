62dca83e8e84341804eebd1f385c95254395e48e
Mikerah <mikerah14@gmail.com>
2016-09-25 08:47:12 -0400

    added skeleton code for genres_playlist.py

---
+++ genre_explorer/genres/genres_playlist.py
@@ -0,0 +1,9 @@
+from .models import Genre
+from .genres_scraper import create_genres_dictionary
+
+def create_genres_from_dictionary():
+    """
+    Returns a sorted list of Genre objects from the dictionary of genres
+    """
+    pass
+    
