dc67077c88f57aaa3a7e3d5427d4a8882e92cb16
Mikerah <mikerah14@gmail.com>
2016-10-01 14:28:40 -0400

    Removed API KEY and added a placeholder in constants.py

---
+++ genre_explorer/genres/constants.py
@@ -0,0 +1,8 @@
+import string
+
+API_KEY = " YOUR_API_KEY "
+YOUTUBE_API_SERVICE_NAME = "youtube"
+YOUTUBE_API_VERSION = "v3"
+
+LIST_OF_LETTERS = list(string.ascii_uppercase)
+LIST_OF_LETTERS.append("#")
