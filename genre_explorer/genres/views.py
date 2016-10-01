991390874ec1c70c415b0ac26d04c4ef181c042c
Mikerah <mikerah14@gmail.com>
2016-09-30 21:13:26 -0400

    rewrote code in index.html, removed a line of code in views.py

--- genre_explorer/genres/views.py
+++ genre_explorer/genres/views.py
@@ -4,6 +4,4 @@ from .constants import LIST_OF_LETTERS
 
 def index(request):
     genres = create_genres_from_dictionary()
-    
     return render(request, 'genres/index.html', {'genres': genres, 'letters': list_of_letters})
-    #return render(request, 'genres/index.html', {'letters': LIST_OF_LETTERS})
